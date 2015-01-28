f27a474f5814762647b47350de8521a86ac8d7ba
Ajay Jagad <ajayjagad@Ajays-Air.Home>
2015-01-29 01:58:40 +0530

    init

---
+++ config/initializers/constants.rb
@@ -0,0 +1,11 @@
+module ProjectApp
+  DEFAULT_LANG        = "eng"
+  SMTP_ADDRESS        = "smtp.gmail.com"
+  SMTP_PORT           = "587"
+  SMTP_AUTHENTICATION = "plain"
+  SMTP_USERNAME       = "xyz1234@gmail.com"
+  SMTP_PASSWORD       = "1234ffff"
+  SMTP_DOMAIN         = "gmail.com"
+  DEFAULT_MAIL_FROM   = "xyz1234@gmail.com"
+  BASE_URL            = "http://localhost:3000/"
+end
